59252af40f92fff062a934ac3584f866deace32b
Harborth, Allen <allenharborth9835@gmail.com>
2021-08-15 21:18:18 -0500

    update html template

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const inquirer = require('inquirer')
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
 
-//const pageHTML = generatePage(name, github);
-
-//fs.writeFile('./index.html',pageHTML , err =>{
-  //if(err) throw err;
-
-  //console.log('Portfolio complete! Check out index.html to see the output!')
-//});
 const promptUser = () => {
     return inquirer.prompt([
     {
@@ -138,10 +131,14 @@ const promptProject = portfolioData => {
         }
     });
 };
-
 promptUser()
     .then(promptProject)
     .then(portfolioData =>{
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html',pageHTML , err =>{
+        if(err) throw err;
+
+        });
     })
     
